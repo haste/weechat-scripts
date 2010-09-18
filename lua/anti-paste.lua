bdf8ff2c16b53aa3b4ecb64e1e09a3967598508a
Trond A Ekseth <troeks@gmail.com>
2010-09-18 20:57:41 +0200

    A quick script to block myself from pasting single-lines no IRC.

---
+++ lua/anti-paste.lua
@@ -0,0 +1,49 @@
+-- meh
+local gettime = require'socket'.gettime
+
+weechat.register('anti-paste', 'haste', '1.0', 'GPL3', 'Block single-line pastes.', '', '')
+
+local inputTime
+function hook_command_run(src)
+	if(src == 'anti-paste') then
+		if(inputTime and inputTime < .005) then
+			inputTime = nil
+			return weechat.WEECHAT_RC_OK_EAT()
+		end
+	end
+
+	return weechat.WEECHAT_RC_OK()
+end
+
+local inputStart
+function signal(src, event, key)
+	-- Reset the timer when we switch buffers.
+	if(event == 'buffer_switch') then
+		inputStart = nil
+	elseif(key == '^M' and inputStart) then
+		inputTime = gettime() - inputStart
+		inputStart = nil
+	elseif(key:sub(1,1) ~= '^' and not inputStart) then
+		inputTime = nil
+		inputStart = gettime()
+	end
+
+	return weechat.WEECHAT_RC_OK()
+end
+
+function reset(src, buffer, event)
+	if(src == 'anti-paste') then
+		inputStart = nil
+	end
+
+	return weechat.WEECHAT_RC_OK()
+end
+
+weechat.hook_signal('key_pressed', 'signal', '')
+weechat.hook_signal('buffer_switch', 'signal', '')
+weechat.hook_command_run('/input return', 'hook_command_run', 'anti-paste')
+
+weechat.hook_command_run('/input history_next', 'reset', 'anti-paste')
+weechat.hook_command_run('/input history_previous', 'reset', 'anti-paste')
+weechat.hook_command_run('/input move_next_char', 'reset', 'anti-paste')
+weechat.hook_command_run('/input move_previous_char', 'reset', 'anti-paste')
